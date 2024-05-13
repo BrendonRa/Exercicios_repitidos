dcd04d22b9f383f55203ffc57459ad98dcd7977e
Brendon Rabelo dos Santos <rabelobrendon@gmail.com>
2024-05-13 07:47:04 -0300

    terminado

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.Write("Digite primeiro número: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite segundo número: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            if (n1 < n2)
+            {
+                for (int i = n1; i <= n2; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else if (n1 > n2)
+            {
+                for (int i = n2; i <= n1; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Números inválidos");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Digite um número: ");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(num + " X " + i + " = " + num * i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
